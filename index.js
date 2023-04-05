4b66ae7ee8f28ae6dbb9d1861fde61edaafd24de
dcs1997 <dhirajsalvi099@gmail.com>
2023-04-06 01:20:56 +0530

    day1

--- index.js
+++ index.js
@@ -1,39 +1,75 @@
-let resButton = document.getElementById("resume-button-2");
-resButton.addEventListener("click", ()=>{
-     window.open("https://drive.google.com/file/d/1GgP3iurT80s1oZq88EeYeGyXPJC_n_FU/view?usp=sharing", "_blank");
-})
-let resButton2 = document.getElementById("resume-button-1");
-resButton2.addEventListener("click", ()=>{
-     window.open("https://drive.google.com/file/d/1GgP3iurT80s1oZq88EeYeGyXPJC_n_FU/view?usp=sharing", "_blank");
-})
-
-
-var navbar = document.getElementById('nav-menu');
-navbar.classList.remove('navBG');
-
-function changNavBG(){
-    var navbar = document.getElementById('nav-menu');
-    var scrollValue = window.scrollY;
-    console.log(scrollValue+ " scroll")
-    if(scrollValue > 150){
-        navbar.classList.add('navBG');
-    }else navbar.classList.remove('navBG');
-
-}
-
-window.addEventListener('scroll',changNavBG);
-
-
-
-const li = document.querySelectorAll('.nav-link');
-const sec = document.querySelectorAll("section");
-
-function activeMenu(){
-    let len = sec.length;
-    while(--len && window.scrollY + 97 < sec[len].offsetTop){}
-    li.forEach(itx => itx.classList.remove("active"));
-    li[len].classList.add("active");
-}
-
-activeMenu();
-window.addEventListener("scroll", activeMenu);
+$(document).ready(function () {
+    $(window).scroll(function () {
+      // sticky navbar on scroll script
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+  
+      // scroll-up button show/hide script
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
+    });
+  
+    // slide-up script
+    $(".scroll-up-btn").click(function () {
+      $("html").animate({ scrollTop: 0 });
+      // removing smooth scroll on slide-up button click
+      $("html").css("scrollBehavior", "auto");
+    });
+  
+    $(".navbar .menu li a").click(function () {
+      // applying again smooth scroll on menu items click
+      $("html").css("scrollBehavior", "smooth");
+    });
+  
+    // toggle menu/navbar script
+    $(".menu-btn").click(function () {
+      $(".navbar .menu").toggleClass("active");
+      $(".menu-btn i").toggleClass("active");
+    });
+  
+  
+    // owl carousel script
+    $(".carousel").owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        600: {
+          items: 2,
+          nav: false,
+        },
+        1000: {
+          items: 3,
+          nav: false,
+        },
+      },
+    });
+  });
+  
+  
+  //change the file here
+  document.getElementById("resume-button-1").onclick = ()=>{
+    window.open("https://drive.google.com/file/d/1GgP3iurT80s1oZq88EeYeGyXPJC_n_FU/view?usp=sharing",`_blank`)
+  }
+  
+  document.getElementById("resume-button-2").onclick = ()=>{
+    window.open("https://drive.google.com/file/d/1GgP3iurT80s1oZq88EeYeGyXPJC_n_FU/view?usp=sharing",`_blank`)
+  }
+  
+  GitHubCalendar(".calendar", "dcs1997", {
+    responsive: true,
+    global_stats: false,
+    tooltips: true,
+  });
