556557efb3141984c83d941b9d185a2870b0af1b
dcs1997 <dhirajsalvi099@gmail.com>
2023-04-05 16:48:03 +0530

    day1

---
+++ index.js
@@ -0,0 +1,39 @@
+let resButton = document.getElementById("resume-button-2");
+resButton.addEventListener("click", ()=>{
+     window.open("https://drive.google.com/file/d/1GgP3iurT80s1oZq88EeYeGyXPJC_n_FU/view?usp=sharing", "_blank");
+})
+let resButton2 = document.getElementById("resume-button-1");
+resButton2.addEventListener("click", ()=>{
+     window.open("https://drive.google.com/file/d/1GgP3iurT80s1oZq88EeYeGyXPJC_n_FU/view?usp=sharing", "_blank");
+})
+
+
+var navbar = document.getElementById('nav-menu');
+navbar.classList.remove('navBG');
+
+function changNavBG(){
+    var navbar = document.getElementById('nav-menu');
+    var scrollValue = window.scrollY;
+    console.log(scrollValue+ " scroll")
+    if(scrollValue > 150){
+        navbar.classList.add('navBG');
+    }else navbar.classList.remove('navBG');
+
+}
+
+window.addEventListener('scroll',changNavBG);
+
+
+
+const li = document.querySelectorAll('.nav-link');
+const sec = document.querySelectorAll("section");
+
+function activeMenu(){
+    let len = sec.length;
+    while(--len && window.scrollY + 97 < sec[len].offsetTop){}
+    li.forEach(itx => itx.classList.remove("active"));
+    li[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
